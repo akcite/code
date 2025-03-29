351f34afb89a102e3082bf6e2a5022260efe8814
Alan Zhang <akcite@gmail.com>
2025-03-29 07:37:32 -0400

    Update algorithm.cpp

--- cpp/algorithm.cpp
+++ cpp/algorithm.cpp
@@ -204,4 +204,37 @@ int main() {
 }
 
 /////////////////////////////////////////////////////////////////////////////////////
+// unique
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+bool myfunction( int i, int j) {
+  return (i == j);
+}
+
+int main() {
+  int myints[] = {10, 20, 20, 20, 30, 30, 20, 20, 10};
+  std::vector<int> myvector(myints, myints + 9);
+
+  std::vector<int>::iterator it;
+  it = std::unique(myvector.begin(), myvector.end());
+
+  myvector.resize(std::distance(myvector.begin(), it));
+
+  std::unique(myvector.begin(), myvector.end(), myfunction);
+
+  std::cout << "myvector contains:";
+  for (it = myvector.begin(); it != myvector.end(); ++it) {
+    std::cout << ' ' << *it;
+  }
+  std::cout << std::endl;
+
+  return 0;
+}
+
+// 10 20 30 20 10
+
+/////////////////////////////////////////////////////////////////////////////////////
 
