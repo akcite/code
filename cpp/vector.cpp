405b418f7d10adc2dd7f9371555049b8b7fd06fb
Alan Zhang <akcite@gmail.com>
2025-04-02 11:10:20 -0400

    Update vector.cpp

--- cpp/vector.cpp
+++ cpp/vector.cpp
@@ -147,3 +147,25 @@ myvector.erase (myvector.begin()+5);
 myvector.erase (myvector.begin(),myvector.begin()+3);
 
 /////////////////////////////////////////////////////////////////////////////////////
+// std::find
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main() {
+    std::vector<int> numbers = {10, 20, 30, 40, 50};
+    int target = 30;
+
+    auto it = std::find(numbers.begin(), numbers.end(), target);
+
+    if (it != numbers.end()) {
+        std::cout << "Element found: " << *it << std::endl;
+    } else {
+        std::cout << "Element not found." << std::endl;
+    }
+
+    return 0;
+}
+
+/////////////////////////////////////////////////////////////////////////////////////
