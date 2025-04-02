2d209533625727b8d052ea9383ce51a6c7c8e402
Alan Zhang <akcite@gmail.com>
2025-04-02 11:35:55 -0400

    Update algorithm.cpp

--- cpp/algorithm.cpp
+++ cpp/algorithm.cpp
@@ -239,4 +239,26 @@ int main() {
 // 10 20 30 
 
 /////////////////////////////////////////////////////////////////////////////////////
+// std::find
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main() {
+    std::vector<int> numbers = {10, 20, 30, 40, 50};
+    int target = 30;
+
+    auto it = std::find(numbers.begin(), numbers.end(), target);
+
+    if (it != numbers.end()) {
+        std::cout << "Element found: " << *it << std::endl;
+    } else {
+        std::cout << "Element not found." << std::endl;
+    }
+
+    return 0;
+}
+
+/////////////////////////////////////////////////////////////////////////////////////
 
